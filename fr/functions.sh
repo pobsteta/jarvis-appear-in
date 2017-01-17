c5388b052e78bbee17afdab14492ab8d7ef4ac6b
Pascal Obstetar <pascal.obstetar@gmail.com>
2017-01-17 21:29:39 +0100

    maj all

--- fr/functions.sh
+++ fr/functions.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 # Here you can create functions which will be available from the commands file
 # You can also use here user variables defined in your config file
+
+jv_pg_ap_search ()
+{
+
+#room definition
+local APPEAR_IN_QUERY="https://appear.in/$appear_room"
+
+#open defaut web browser with the appear.in room
+chromium-browser %U $APPEAR_IN_QUERY
+
+}
